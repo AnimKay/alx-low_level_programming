7243d40b166f2e12a2d6f7b31b84b2bd0250a270
Animkay <Kelvinanim2000@gmail.com>
2022-12-12 11:19:38 -0800

    Update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,6 +2,7 @@
 
 /**
  * main - returns all possible combinations of two digit numbers
+ *
  * Return: Always 0 (Success)
  */
 
@@ -11,16 +12,16 @@ int main(void)
 
 	for (i = 0; j < 100; j++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
-			if (j != i)
+			if (i < j)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i *100 + j != 9899)
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
