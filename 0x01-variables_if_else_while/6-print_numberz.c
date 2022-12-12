c8691b297727b632911b0b76f5699f57281c71dc
Animkay <Kelvinanim2000@gmail.com>
2022-12-12 04:44:20 -0800

    Added 6-print_numberz file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return all single integers from zero to ten
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
