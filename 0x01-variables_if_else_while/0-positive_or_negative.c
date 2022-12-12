3bf14283b5771ec1e47e4fd9cc165b992637dcac
Animkay <Kelvinanim2000@gmail.com>
2022-12-12 03:25:49 -0800

    added 0-positive_or_negative file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,13 @@
 
 /**
  * main -main function to generate a random number 
- *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
+
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
