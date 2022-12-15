b40719b4cb3bf043776b76a408e7f40110860405
Animkay <Kelvinanim2000@gmail.com>
2022-12-15 04:04:01 -0800

    Added all task files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: parameter
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numners
+ * @a: parameter
+ * @b: parameter
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
